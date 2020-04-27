18a34eca1923dfd2180ce685b4ed882893f22f90
Afsane Mahdaviazad <afsane.mahdaviazad@symanto.net>
2020-04-27 16:01:58 +0200

    final changes

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
 # Introduction 
- slack bot which intract with our deeplearning api
+ slack bot which intracts with our deeplearning api
 
 # Getting Started
 
 1.	before run the node.js, rename .env.test file to .env file and put your data(like api token and bot token) in it
-2.	if any package is missed and have error, find the name in packages.json  and install them from 
+2.	if any package is missed and have error, find the name in packages.json and install them from 
 
-
-# Build and Test
-TODO: Describe and show how to build your code and run the tests. 
-
-# Contribute
-TODO: Explain how other users and developers can contribute to make your code better. 
 
 If you want to learn more about creating good readme files then refer the following [guidelines](https://docs.microsoft.com/en-us/azure/devops/repos/git/create-a-readme?view=azure-devops). You can also seek inspiration from the below readme files:
 - [ASP.NET Core](https://github.com/aspnet/Home)
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const qs = require('qs');
 require('dotenv').config();
 
 const rawBodyBuffer = (req, res, buf, encoding) => {
@@ -12,20 +11,19 @@ const rawBodyBuffer = (req, res, buf, encoding) => {
     req.rawBody = buf.toString(encoding || 'utf8');
   }
 };
-app.use(bodyParser.urlencoded({verify: rawBodyBuffer, extended: true }));
+app.use(bodyParser.urlencoded({ verify: rawBodyBuffer, extended: true }));
 app.use(bodyParser.json({ verify: rawBodyBuffer }));
 
-const apiUrl = 'https://slack.com/api';
 
 /* Handling events */
 
 
 app.get('/health', (req, res) => {
-  res.send('.');
+  res.send('Server is running');
 });
 
 app.post('/events', (req, res) => {
-    
+
   // App setting validation
   if (req.body.type === 'url_verification') {
     res.send(req.body.challenge);
@@ -33,65 +31,62 @@ app.post('/events', (req, res) => {
 
   // Events 
 
-   else if (req.body.type === 'event_callback') {
+  else if (req.body.type === 'event_callback') {
     res.sendStatus(200);
 
-    const {bot_id, text, user, channel} = req.body.event; 
-    if(!text) return;
+    const { bot_id, text, user, channel } = req.body.event;
+    if (!text) return;
 
     let regex = /(^\/)/;
-    if(bot_id || regex.test(text)) return;
-    if(req.body.event.subtype==='channel_join' || req.body.event.subtype==='bot_add' || req.body.event.subtype==='bot_remove' ) return;
+    if (bot_id || regex.test(text)) return;
+    if (req.body.event.subtype === 'channel_join' || req.body.event.subtype === 'bot_add' || req.body.event.subtype === 'bot_remove') return;
     analyzeTextByDLApi(text, user, channel);
-    
+
   }
-  
+
 });
-function analyzeTextByDLApi(text, user, channel)
-{
+function analyzeTextByDLApi(text, user, channel) {
 
-      var jsonString = JSON.stringify({
-        id:  "",
-        language: "en",
-        text: text
-      });
-      var input= "["+ jsonString + "]";       
+  var jsonString = JSON.stringify({
+    id: "",
+    language: "en",
+    text: text
+  });
+  var input = "[" + jsonString + "]";
 
-      var sentimentUrl=process.env.DL_SENTIMENT;
-      var config = {
-        method: 'post',
-        headers:  {
-            "Content-Type": "application/json",
-            "API_KEY" :process.env.API_KEY
-        }
-      };
-      var final = "";
-      var username= "";
-      axios.post(sentimentUrl , input, config)
-      .then(function (response) {
-        var result = JSON.stringify(response.data);
-        var finalResult= JSON.parse(result);
-         final = finalResult[0].predictions[0].prediction ;
-         console.log(final);
-         
-         axios.post('https://slack.com/api/users.info?token='+ process.env.SLACK_ACCESS_TOKEN +'&user='+ user +'&pretty=1')
-          .then(function (response) {
-            username = response.data.user.name;
-            var emoji = "";
-            if(final==='positive')
-            {
-              emoji = ':slightly_smiling_face:';
-            }
-            else if(final==='negative'){
-              emoji = ':white_frowning_face:';
-            }
-            axios.post('https://slack.com/api/chat.postMessage?token='+ process.env.SLACK_ACCESS_TOKEN +'&channel=' +channel+ '&text='+username +' sent a '+final+ ' message ' + emoji +'&pretty=1');
-          }) 
-      })   
+  var sentimentUrl = process.env.DL_SENTIMENT;
+  var config = {
+    method: 'post',
+    headers: {
+      "Content-Type": "application/json",
+      "API_KEY": process.env.API_KEY
+    }
+  };
+  var final = "";
+  var username = "";
+  axios.post(sentimentUrl, input, config)
+    .then(function (response) {
+      var result = JSON.stringify(response.data);
+      var finalResult = JSON.parse(result);
+      final = finalResult[0].predictions[0].prediction;
+
+      axios.post('https://slack.com/api/users.info?token=' + process.env.SLACK_ACCESS_TOKEN + '&user=' + user + '&pretty=1')
+        .then(function (response) {
+          username = response.data.user.name;
+          const emoji;
+          if (final === 'positive') {
+            emoji = ':slightly_smiling_face:';
+          }
+          else if (final === 'negative') {
+            emoji = ':white_frowning_face:';
+          }
+          axios.post('https://slack.com/api/chat.postMessage?token=' + process.env.SLACK_ACCESS_TOKEN + '&channel=' + channel + '&text=' + username + ' sent a ' + final + ' message ' + emoji + '&pretty=1');
+        })
+    })
 }
 
 
-const server = app.listen(process.env.PORT || 80 , () => {
+const server = app.listen(process.env.PORT || 80, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 
 });
